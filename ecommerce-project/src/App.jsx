65b3b3e50c3d09ef4f5044acc90c35fae5aebbc4
lim cheaheng <limcheaheng168@gmail.com>
2026-05-15 11:16:31 +0700

    Update link to match the router

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <Routes>
       <Route index element={<HomePage />} />
-      <Route path='/checkout' element={<CheckoutPage />} />
+      <Route path='checkout' element={<CheckoutPage />} />
     </Routes>
   );
 }
