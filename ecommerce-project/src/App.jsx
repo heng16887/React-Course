24c371113adea67e5b821625dc5ed7a9d6011463
lim cheaheng <limcheaheng168@gmail.com>
2026-05-18 08:36:58 +0700

    change tracking page HTML into react jsx

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -3,6 +3,7 @@ import { HomePage } from './pages/HomePage';
 // Exercise 6B
 import { CheckoutPage } from './pages/checkout/CheckoutPage';
 import { OrdersPage } from './pages/OrdersPage';
+import { TrackingPage } from './pages/TrackingPage';  
 import './App.css'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Route index element={<HomePage />} />
       <Route path='checkout' element={<CheckoutPage />} />
       <Route path='orders' element={<OrdersPage />} />
+      <Route path='tracking' element={<TrackingPage />} />
     </Routes>
   );
 }
