40a1992d24e7144299d3029dcca7217d205455a1
lim cheaheng <limcheaheng168@gmail.com>
2026-05-18 14:44:26 +0700

    create notFound page

---
+++ ecommerce-project/src/components/NotFoundPage.jsx
@@ -0,0 +1,12 @@
+import './NotFoundPage.css';
+
+export function NotFoundPage() {
+  return (
+    <>
+      <div className="not-found-page-container">
+        <span className="number-not-found-page">404</span>
+        <p className="not-found-page-text">Not Found Page</p>
+      </div>
+    </>
+  );
+}
