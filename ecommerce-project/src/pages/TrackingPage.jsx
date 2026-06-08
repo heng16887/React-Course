e4a0f320beec05ecaf60875dacefdb0a6b85440a
lim cheaheng <limcheaheng168@gmail.com>
2026-06-08 10:20:22 +0700

    update the calculating of the progress bar

--- ecommerce-project/src/pages/TrackingPage.jsx
+++ ecommerce-project/src/pages/TrackingPage.jsx
@@ -1,16 +1,51 @@
 import { Link, useParams } from "react-router";
+import { useEffect, useState } from "react";
+import dayjs from "dayjs";
+import axios from "axios";
 import { Header } from "../components/Header";
 import "./TrackingPage.css";
 
 export function TrackingPage({cart}) {
+  // Exercise 7k
+  const [order, setOrder] = useState(null);
   const { orderId, productId } = useParams();
+
+  useEffect(() => {
+    const orderData = async () => {
+      const response = await axios.get(
+        `/api/orders/${orderId}?expand=products`,
+      );
+      setOrder(response.data);
+    };
+
+    orderData();
+  }, [orderId]);
+
+  if (!order) {
+    return null;
+  }
+
+  const orderProduct = order.products.find((orderProduct) => {
+    return orderProduct.product.id === productId;
+  });
+  // Exercise 7l: get the total time for delivery
+  const totalDeliveryTimeMs = orderProduct.estimatedDeliveryTImeMs - order.orderTimeMs;
+  // Exercise 7l: get the time passed since the order was placed
+  const timePassedMs = dayjs().valueOf() - order.orderTimeMs;
+  // Exercise 7l: calculate the percentage of delivery progress
+  let deliveryPercent = (timePassedMs / totalDeliveryTimeMs) * 100;
+  console.log(deliveryPercent);
+
+  if(deliveryPercent > 100) {
+    deliveryPercent = 100;
+  }
 
   return (
     <>
       <link rel="icon" href="/tracking-favicon.png" />
       <title>Tracking</title>
 
-      <Header cart={cart}/>
+      <Header cart={cart} />
 
       <div className="tracking-page">
         <div className="order-tracking">
@@ -18,18 +53,15 @@ export function TrackingPage({cart}) {
             View all orders
           </Link>
 
-          <div className="delivery-date">Arriving on Monday, June 13</div>
-
-          <div className="product-info">
-            Black and Gray Athletic Cotton Socks - 6 Pairs
+          <div className="delivery-date">
+            Arriving on {dayjs(order.deliveryDate).format("dddd, MMMM D")}
           </div>
 
-          <div className="product-info">Quantity: 1</div>
+          <div className="product-info">{orderProduct.product.name}</div>
 
-          <img
-            className="product-image"
-            src="images/products/athletic-cotton-socks-6-pairs.jpg"
-          />
+          <div className="product-info">Quantity: {orderProduct.quantity}</div>
+
+          <img className="product-image" src={orderProduct.product.image} />
 
           <div className="progress-labels-container">
             <div className="progress-label">Preparing</div>
@@ -38,7 +70,9 @@ export function TrackingPage({cart}) {
           </div>
 
           <div className="progress-bar-container">
-            <div className="progress-bar"></div>
+            <div className="progress-bar"
+                 style={{width: `${deliveryPercent}%`}}>
+            </div>
           </div>
         </div>
       </div>
