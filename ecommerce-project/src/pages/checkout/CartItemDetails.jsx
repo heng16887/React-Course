e8ca9a89f88a2d8be530a769aecfe955e83fddf9
lim cheaheng <limcheaheng168@gmail.com>
2026-06-22 09:08:46 +0700

    make update button interactive

--- ecommerce-project/src/pages/checkout/CartItemDetails.jsx
+++ ecommerce-project/src/pages/checkout/CartItemDetails.jsx
@@ -1,12 +1,21 @@
+import { useState } from "react";
 import { formatMoney } from "../../utils/money";
+import './CartItemDetails.css'
 import axios from "axios";
 
 export function CartItemDetails({ cartItem, loadCart })
 {
+  const [updateQuantity, setUpdatedQuantity] = useState(false);
+
   const deleteCartItem = async () => {
     await axios.delete(`/api/cart-items/${cartItem.productId}`);
     await loadCart();
   }; 
+
+  // Exercise 8f
+  function updateQuantityInput(){
+    setUpdatedQuantity(!updateQuantity);
+  }
 
   return (
     <>
@@ -19,12 +28,30 @@ export function CartItemDetails({ cartItem, loadCart })
         </div>
         <div className="product-quantity">
           <span>
-            Quantity:{" "}
-            <span className="quantity-label">{cartItem.quantity}</span>
+            Quantity:
+            {updateQuantity ? (
+              <>
+                <input className="textbox" type="text" />
+                <span className="quantity-label">{cartItem.quantity}</span>
+              </>
+            ) : (
+              <>
+                <span className="quantity-label">{cartItem.quantity}</span>
+              </>
+            )}
           </span>
-          <span className="update-quantity-link link-primary">Update</span>
-          <span className="delete-quantity-link link-primary"
-          onClick={deleteCartItem}>Delete</span>
+          <span
+            className="update-quantity-link link-primary"
+            onClick={updateQuantityInput}
+          >
+            Update
+          </span>
+          <span
+            className="delete-quantity-link link-primary"
+            onClick={deleteCartItem}
+          >
+            Delete
+          </span>
         </div>
       </div>
     </>
