1899c0099578f5b46123b70a8b5cdcf071d5a470
lim cheaheng <limcheaheng168@gmail.com>
2026-06-22 11:37:11 +0700

    make search bar and button interactive

--- ecommerce-project/src/components/Header.jsx
+++ ecommerce-project/src/components/Header.jsx
@@ -1,12 +1,23 @@
-import { NavLink } from 'react-router';
+import { NavLink } from "react-router";
+import { useState } from "react";
 import Logo from "../assets/images/logo-white.png";
-import MobileLogo from "../assets/images/mobile-logo.png";  
+import MobileLogo from "../assets/images/mobile-logo.png";
 import SearchIcon from "../assets/images/icons/search-icon.png";
 import CartIcon from "../assets/images/icons/cart-icon.png";
-import './header.css';
+import "./header.css";
 
 export function Header({ cart }) {
   let totalQuantity = 0;
+  // Exercise 8j
+  const [searchText, setSearchingText] = useState('');
+
+  const searchingButton = () => {
+    console.log(searchText);
+  };
+  
+  const searchInput = (event) => {
+    setSearchingText(event.target.value);
+  }
 
   cart.forEach((cartItem) => {
     totalQuantity += cartItem.quantity;
@@ -22,9 +33,9 @@ export function Header({ cart }) {
       </div>
 
       <div className="middle-section">
-        <input className="search-bar" type="text" placeholder="Search" />
+        <input className="search-bar" type="text" placeholder="Search" onChange={searchInput}/>
 
-        <button className="search-button">
+        <button className="search-button" onClick={searchingButton} value={searchText}>
           <img className="search-icon" src={SearchIcon} />
         </button>
       </div>
@@ -36,9 +47,7 @@ export function Header({ cart }) {
 
         <NavLink className="cart-link header-link" to="/checkout">
           <img className="cart-icon" src={CartIcon} />
-          <div className="cart-quantity">
-            {totalQuantity}
-          </div>
+          <div className="cart-quantity">{totalQuantity}</div>
           <div className="cart-text">Cart</div>
         </NavLink>
       </div>
