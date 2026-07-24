77e678c877c3bf4ba6443414c8f9beceaf527253
lim cheaheng <limcheaheng168@gmail.com>
2026-07-24 11:32:16 +0700

    Use beforeEach to remove duplication

--- ecommerce-project/src/pages/home/Product.test.jsx
+++ ecommerce-project/src/pages/home/Product.test.jsx
@@ -1,4 +1,4 @@
-import { it, expect, describe, vi} from "vitest";
+import { it, expect, describe, vi, beforeEach} from "vitest";
 import {render, screen} from '@testing-library/react';
 // screen = check the fake web page
 import userEvent from '@testing-library/user-event';
@@ -8,15 +8,19 @@ import { Product } from "./Product";
 vi.mock('axios');
 
 describe('Product component', () => {
-  // this is Integration test
-  it('displays the product details correctly', () => {
-    // to testing component, we need to use render()
-    // render() = display the component on the page
+  // to testing component, we need to use render()
+  // render() = display the component on the page
 
-    // However, we need to install a packages 'npm install --save-dev @testing-library/react@16.3.0 @testing-library/jest-dom@6.6.3 @testing-library/user-event@14.6.1 jsdom@26.1.0'
-    // and then config and set up the file (create a file[vitest.config.js] and [setupTests.js])
+  // However, we need to install a packages 'npm install --save-dev @testing-library/react@16.3.0 @testing-library/jest-dom@6.6.3 @testing-library/user-event@14.6.1 jsdom@26.1.0'
+  // and then config and set up the file (create a file[vitest.config.js] and [setupTests.js])
 
-    const product = {
+  // MOCK = creaet a fake version of this function
+  let product;
+  // vi.fn() = creaet a fake function that doesn't do anything
+  let loadCart;
+
+  beforeEach(() => {
+    product = {
       id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
       image: "images/products/athletic-cotton-socks-6-pairs.jpg",
       name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
@@ -27,10 +31,12 @@ describe('Product component', () => {
       priceCents: 1090,
       keywords: ["socks", "sports", "apparel"],
     };
-    // MOCK = creaet a fake version of this function
-    // vi.fn() = creaet a fake function that doesn't do anything
-    const loadCart = vi.fn();
-    render(<Product product={product} loadCart={loadCart}/>);
+    loadCart = vi.fn();
+  });
+
+  // this is Integration test
+  it("displays the product details correctly", () => {
+    render(<Product product={product} loadCart={loadCart} />);
 
     expect(
       screen.getByText("Black and Gray Athletic Cotton Socks - 6 Pairs"),
@@ -48,25 +54,11 @@ describe('Product component', () => {
       "images/ratings/rating-45.png",
     );
 
-    expect(
-      screen.getByText('87')
-    ).toBeInTheDocument();
+    expect(screen.getByText("87")).toBeInTheDocument();
   });
 
   // this is user interaction test
-  it('adds a product to the cart', async () => {
-    const product = {
-      id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-      image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-      name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-      rating: {
-        stars: 4.5,
-        count: 87,
-      },
-      priceCents: 1090,
-      keywords: ["socks", "sports", "apparel"],
-    };
-    const loadCart = vi.fn();
+  it("adds a product to the cart", async () => {
     render(<Product product={product} loadCart={loadCart} />);
 
     const user = userEvent.setup();
@@ -75,7 +67,7 @@ describe('Product component', () => {
     await user.click(addToCartButton); // this line is asychronous code (it returns a Promise)
     expect(axios.post).toHaveBeenCalledWith("/api/cart-items", {
       productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-      quantity: 1
+      quantity: 1,
     });
     expect(loadCart).toHaveBeenCalled();
   });
