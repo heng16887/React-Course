fd0e969088c2095435339adc72170ecd7035c9d5
lim cheaheng <limcheaheng168@gmail.com>
2026-07-24 10:45:46 +0700

    CREATE unit test for format money

---
+++ ecommerce-project/src/utils/money.test.js
@@ -0,0 +1,16 @@
+import { it, expect, describe } from 'vitest'; 
+// (it) create a test
+// describe = group of test
+import { formatMoney } from './money';
+
+describe('format Money', () => {
+  it('formats 1999 cents as $19.99', () => {
+    // expect = check if the result is correct
+    expect(formatMoney(1999)).toBe('$19.99');
+  }); // passed
+  
+  it('displays 2 decimal', () => {
+    expect(formatMoney(1090)).toBe('$10.90');
+    expect(formatMoney(100)).toBe('$1.00');
+  }); // passed
+}); 
