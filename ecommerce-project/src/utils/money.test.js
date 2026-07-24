1ec1ab83ee315673c66941c6e4cb0aff0ffcfbf3
lim cheaheng <limcheaheng168@gmail.com>
2026-07-24 13:41:38 +0700

    check the negative format number

--- ecommerce-project/src/utils/money.test.js
+++ ecommerce-project/src/utils/money.test.js
@@ -18,4 +18,9 @@ describe('format Money', () => {
   it('work with the number 0', () => {
     expect(formatMoney(0)).toBe("$0.00");
   });
+
+  it('work with negative number', () => {
+    expect(formatMoney(-999)).toBe('$-9.99')
+    expect(formatMoney(-100)).toBe('$-1.00')
+  });
 }); 
