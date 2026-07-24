b1dabce9373a2839ab6cfd89dd55d449cb8843f8
lim cheaheng <limcheaheng168@gmail.com>
2026-07-24 13:42:18 +0700

    add comment exercise

--- ecommerce-project/src/utils/money.test.js
+++ ecommerce-project/src/utils/money.test.js
@@ -19,6 +19,7 @@ describe('format Money', () => {
     expect(formatMoney(0)).toBe("$0.00");
   });
 
+  // 9b: test money with negative number
   it('work with negative number', () => {
     expect(formatMoney(-999)).toBe('$-9.99')
     expect(formatMoney(-100)).toBe('$-1.00')
